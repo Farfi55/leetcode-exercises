4e8db125ecf7ffcdadd1db4b501f868489e465f0
Alessio Farfaglia <djalexy555@gmail.com>
2021-12-30 17:15:42 +0100

    [16] 3Sum Closest (v. 2)

--- medium/16_3_sum_closest.cpp
+++ medium/16_3_sum_closest.cpp
@@ -15,18 +15,19 @@ using namespace std;
 class Solution {
 public:
 	int threeSumClosest(vector<int>& nums, int target) {
-		int closest = nums[0] + nums[1] + nums[2];
 		sort(nums.begin(), nums.end());
 		const int n = nums.size();
+		int closest = nums[0] + nums[1] + nums[n - 1];
+
 		for(int i = 0; i < n; i++) {
 			int left = i + 1;
 			int right = n - 1;
 
 			while(left < right) {
 				int sum = nums[i] + nums[left] + nums[right];
-				if(abs(target - sum) < abs(target - closest)) {
+				if(abs(target - sum) < abs(target - closest))
 					closest = sum;
-				}
+
 				if(sum < target) left++;
 				else if(sum > target) right--;
 				else return target;
