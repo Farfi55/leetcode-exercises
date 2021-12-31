85465d75ebb15075c2c10a6390bc7a555f613611
Alessio Farfaglia <djalexy555@gmail.com>
2021-12-31 19:24:18 +0100

    [28] Implement strStr() (v.2)

--- easy/28_implement_str_str.cpp
+++ easy/28_implement_str_str.cpp
@@ -6,26 +6,75 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 // @lc code=start
 class Solution {
+
+	// https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+	void kmp(string patt, vector<int>& lps) {
+		int m = patt.size();
+		int len = 0;
+		int i = 1;
+
+		lps[0] = 0;
+		while(i < m) {
+			if(patt[i] == patt[len]) {
+				len++;
+				lps[i] = len;
+				i++;
+			}
+			else { // (patt[i] != patt[len])
+				if(len != 0) {
+					len = lps[len - 1];
+				}
+				else { // (len == 0)
+					lps[i] = 0;
+					i++;
+				}
+			}
+		}
+	}
+
 public:
 	int strStr(string haystack, string needle) {
 		const int n = haystack.size();
 		const int m = needle.size();
 
-		for(int i = 0; i <= n - m; i++) {
-			bool found = true;
-			for(int j = 0; found && j < m; j++) {
-				if(needle[j] != haystack[i + j])
-					found = false;
+		if(m == 0) return 0;
+
+		vector<int> lps(m, 0);
+
+		kmp(needle, lps);
+
+		int i = 0, j = 0;
+
+		while(i < n) {
+			if(needle[j] == haystack[i]) {
+				j++;
+				i++;
 			}
-			if(found) return i;
+			if(j == m) {
+				return i - j;
+				// if all matches were needed
+				j = lps[j - i];
+			}
+			else if(i < n && needle[j] != haystack[i]) {
+				if(j != 0)
+					j = lps[j - 1];
+				else i++;
+			}
 		}
 		return -1;
 	}
 };
 // @lc code=end
 
+int main(int argc, char const* argv[])
+{
+	Solution sol;
+	cout << sol.strStr("ABABDABACDABABCABAB", "ABABCABAB");
+	return 0;
+}
