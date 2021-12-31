f090082d3f64e24aa6d5670522e6e9dd2297e8c6
Alessio Farfaglia <djalexy555@gmail.com>
2021-12-31 14:56:31 +0100

    [18] 4Sum (v. 1)

---
+++ medium/18_4_sum.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=18 lang=cpp
+ *
+ * [18] 4Sum
+ */
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		vector<vector<int>> out;
+		sort(nums.begin(), nums.end());
+		const int n = nums.size();
+
+		for(int i = 0; i < n - 3; i++) {
+			for(int j = i + 1; j < n - 2; j++) {
+				// if the first element of the triplet is > 0, 
+				// we won't find any that summed will give 0 anymore
+
+				int front = j + 1;
+				int back = n - 1;
+
+				while(front < back) {
+
+					int64_t sum = (int64_t)nums[i] + nums[j] + nums[front] + nums[back];
+					if(sum < target) front++;
+					else if(sum > target) back--;
+					else {
+						vector<int> quadrutuplet{ nums[i], nums[j], nums[front], nums[back] };
+						out.push_back(quadrutuplet);
+
+						// getting the next value for left and right
+						while(front + 1 < back && nums[front] == quadrutuplet[2]) ++front;
+						while(front < back && nums[back] == quadrutuplet[3]) --back;
+					}
+				}
+				// we skip to the next value for j
+				while(j + 1 < n && nums[j] == nums[j + 1]) j++;
+
+			}
+			// we skip to the next value for i
+			while(i + 1 < n && nums[i] == nums[i + 1]) i++;
+		}
+		return out;
+	}
+};
+// @lc code=end
+
+int main(int argc, char const* argv[])
+{
+	Solution sol;
+	vector<int> nums = { 1,-2,-5,-4,-3,3,3,5 };
+	int target = -11;
+
+	auto out = sol.fourSum(nums, target);
+
+	for(auto& quadrutuplet : out) {
+		cout << '[' << quadrutuplet[0] << ' '
+			<< quadrutuplet[1] << ' '
+			<< quadrutuplet[2] << ' '
+			<< quadrutuplet[3] << "]\n";
+	}
+	return 0;
+}
