af870d6174e752258ec800334fd02227e46b4f54
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-02 16:59:47 +0100

    [189] Rotate Array (v. 3) using std::reverse three times

--- medium/189_rotate_array.cpp
+++ medium/189_rotate_array.cpp
@@ -5,56 +5,51 @@
  */
 
 #include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 /*
 [1,2,3,4,5,6,7] k = 3, n = 7
-[-,-,-,-,-,-,-]
 
-start at: 0 -> 1
-[-,-,-,1,-,-,-] tmp: 4   	0+3=3 (mod 7)
-[-,-,-,1,-,-,4] tmp: 7   	3+3=6 (mod 7)
-[-,-,7,1,-,-,4] tmp: 3   	6+3=2 (mod 7)
-[-,-,7,1,-,3,4] tmp: 6 		2+3=5 (mod 7)
-[-,6,7,1,-,3,4] tmp: 2		5+3=1 (mod 7)
-[-,6,7,1,2,3,4] tmp: 5		1+3=4 (mod 7)
-[5,6,7,1,2,3,4] tmp: 1		4+3=0 (mod 7)
-end-cycle
+[1,2,3,4,5,6,7] IN
+|-------------| reverse (0...n) -> range(0,7)
+[7,6,5,4,3,2,1]
+|-----|			reverse (0...k) -> range(0,3)
+[5,6,7,4,3,2,1]
+	  |-------|	reverse (k...n) -> range(3,7)
+[5,6,7,1,2,3,4] OUT
 
-[5,6,7,1,2,3,4]
+
+same as
+[1,2,3,4,5,6,7]
+
+[1,2,3,4,5,6,7]	IN
+|-------|		reverse (0...n-k) -> range(0, 4)
+[4,3,2,1,5,6,7]
+		|-----|	reverse (n-k...n) -> range(4, 7)
+[4,3,2,1,7,6,5]
+|-------------| reverse (0...n)   -> range(0, 7)
+[5,6,7,1,2,3,4] OUT
 */
 
 // @lc code=start
 class Solution {
 public:
 	void rotate(vector<int>& nums, int k) {
-		const int n = nums.size();
-		k %= n;
-		if(k == 0) return;
+		k %= nums.size();
 
-		int rotated = 0;
-		int start = 0;
-		int curr = 0;
-		int to_be_rotated = nums[start];
-		int tmp = 0;
+		reverse(nums.begin(), nums.end());
+		reverse(nums.begin(), nums.begin() + k);
+		reverse(nums.begin() + k, nums.end());
 
-		while(rotated < n) {
-			curr = start;
-			do {
-				int next = (curr + k) % n;
-				tmp = nums[next];
-				nums[next] = to_be_rotated;
-				to_be_rotated = tmp;
-				curr = next;
-				rotated++;
-			} while(curr != start);
+		// or
 
-			start++;
-			curr = start;
-			to_be_rotated = nums[start];
-		}
+		// reverse(nums.begin(), nums.end() - k);
+		// reverse(nums.end() - k, nums.end());
+		// reverse(nums.begin(), nums.end());
+
 	}
 };
 // @lc code=end
