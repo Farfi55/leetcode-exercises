c4afa729cda558bcd77d63104f588e25e0f2a71f
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-02 16:28:30 +0100

    [189] Rotate Array (v.2) using modular arithmetic, and peeking at the discussions

--- medium/189_rotate_array.cpp
+++ medium/189_rotate_array.cpp
@@ -11,8 +11,18 @@ using namespace std;
 
 /*
 [1,2,3,4,5,6,7] k = 3, n = 7
-[5,6,7]
-[-,-,-,1,2,3,4]
+[-,-,-,-,-,-,-]
+
+start at: 0 -> 1
+[-,-,-,1,-,-,-] tmp: 4   	0+3=3 (mod 7)
+[-,-,-,1,-,-,4] tmp: 7   	3+3=6 (mod 7)
+[-,-,7,1,-,-,4] tmp: 3   	6+3=2 (mod 7)
+[-,-,7,1,-,3,4] tmp: 6 		2+3=5 (mod 7)
+[-,6,7,1,-,3,4] tmp: 2		5+3=1 (mod 7)
+[-,6,7,1,2,3,4] tmp: 5		1+3=4 (mod 7)
+[5,6,7,1,2,3,4] tmp: 1		4+3=0 (mod 7)
+end-cycle
+
 [5,6,7,1,2,3,4]
 */
 
@@ -24,16 +34,27 @@ public:
 		k %= n;
 		if(k == 0) return;
 
-		vector<int> tmp(k, 0);
+		int rotated = 0;
+		int start = 0;
+		int curr = 0;
+		int to_be_rotated = nums[start];
+		int tmp = 0;
 
-		for(int i = 0; i < k; i++)
-			tmp[i] = nums[n - k + i];
+		while(rotated < n) {
+			curr = start;
+			do {
+				int next = (curr + k) % n;
+				tmp = nums[next];
+				nums[next] = to_be_rotated;
+				to_be_rotated = tmp;
+				curr = next;
+				rotated++;
+			} while(curr != start);
 
-		for(int i = n - 1; i >= k; --i)
-			nums[i] = nums[i - k];
-
-		for(int i = 0; i < k; i++)
-			nums[i] = tmp[i];
+			start++;
+			curr = start;
+			to_be_rotated = nums[start];
+		}
 	}
 };
 // @lc code=end
