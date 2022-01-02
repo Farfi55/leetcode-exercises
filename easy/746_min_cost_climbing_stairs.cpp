d0afb2e1c15cfb0bb3d3cce36ee19b71c4ffa9cf
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-02 19:33:42 +0100

    [746] Min Cost Climbing Stairs (v. 2)

--- easy/746_min_cost_climbing_stairs.cpp
+++ easy/746_min_cost_climbing_stairs.cpp
@@ -21,18 +21,22 @@ class Solution {
 	[  -, 105,   5,   5,   4, 102,   3,   2, 100,   1]
 	[  6, 105,   5,   5,   4, 102,   3,   2, 100,   1]
 
-
-
 */
 public:
 	int minCostClimbingStairs(vector<int>& cost) {
 		int n = cost.size();
+		int one_step_cost = cost[n - 2];
+		int two_step_cost = cost[n - 1];
+		int curr_cost = min(one_step_cost, two_step_cost);
+		// f(i) = c(i) + min(f(i+1), f(i+2))
 
 		for(int i = n - 3; i >= 0; --i) {
-			cost[i] += min(cost[i + 1], cost[i + 2]);
+			two_step_cost = one_step_cost;
+			one_step_cost = cost[i] + curr_cost;
+			curr_cost = min(one_step_cost, two_step_cost);
 		}
 
-		return min(cost[0], cost[1]);
+		return curr_cost;
 
 	}
 };
@@ -41,7 +45,7 @@ int main(int argc, char const* argv[])
 {
 	Solution sol;
 
-	vector<int> cost = { 10, 15, 20 };
+	vector<int> cost = { 1, 100,   1,   1,   1, 100,   1,   1, 100,   1 };
 	cout << sol.minCostClimbingStairs(cost);
 	return 0;
 }
