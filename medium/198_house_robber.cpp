b5ff33d3f8ef8de3aeed366b52cd99940840241e
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-03 21:06:26 +0100

    [198] House Robber (v. 2)

--- medium/198_house_robber.cpp
+++ medium/198_house_robber.cpp
@@ -29,7 +29,14 @@ public:
 		if(n == 2) return max(nums[0], nums[1]);
 
 		vector<int> dp(n + 1, -1);
-		return rob(nums, 0, dp);
+		dp[0] = nums[0];
+		dp[1] = max(nums[0], nums[1]);
+
+		for(int i = 2; i < n; i++) {
+			dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+		}
+		dp[n] = max(dp[n - 1], dp[n - 2]);
+		return dp[n];
 	}
 };
 // @lc code=end
