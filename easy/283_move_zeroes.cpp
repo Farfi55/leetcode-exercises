96ee52fb72161e2d89982d679801dc9689392f55
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-03 16:08:12 +0100

    [283] Move Zeroes (v. 2)

--- easy/283_move_zeroes.cpp
+++ easy/283_move_zeroes.cpp
@@ -16,14 +16,14 @@ class Solution {
 public:
 	void moveZeroes(vector<int>& nums) {
 		int n = nums.size();
-		int zeroes = 0;
+		int j = 0;
 
 		for(int i = 0; i < n; i++) {
-			if(nums[i] == 0) zeroes++;
-			else nums[i - zeroes] = nums[i];
+			if(nums[i] != 0)
+				nums[j++] = nums[i];
 		}
-		for(int i = n - zeroes; i < n; i++)
-			nums[i] = 0;
+		while(j < n)
+			nums[j++] = 0;
 	}
 };
 // @lc code=end
