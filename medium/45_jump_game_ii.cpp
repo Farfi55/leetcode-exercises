e0935d829cae56122e0397c0a52dc74dcf467a53
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-04 18:01:44 +0100

    [45] Jump Game II (v. 2) O(n) time

--- medium/45_jump_game_ii.cpp
+++ medium/45_jump_game_ii.cpp
@@ -9,28 +9,56 @@
 
 using namespace std;
 
+/*
+		[2,3,0,1,4] IN
+
+START: 	[0]
+		reach = 0+2
+
+JUMP 1: [2]
+
+		reach = max(range(old_reach, new_reach))
+		reach = max(1+3, 2+0) = 4
+
+JUMP 2: [4]
+		4 >= last_idx -> return JUMPS -> 2
+
+*/
+
+
 // @lc code=start
 class Solution {
-	int getMinimoRaggiungibile(const vector<int> nums, vector<int> dp, int i) {
-		int salti_min = INT_MAX - 1;
-		for(int salto = 1; salto <= nums[i] && (i + salto) < nums.size(); salto++) {
-			salti_min = min(salti_min, dp[i + salto]);
-		}
-		return salti_min;
+	int getMaxReach(const vector<int>& nums, int prev, int curr) {
+		int max_reach = 0;
+		for(int i = prev + 1; i <= curr; i++)
+			max_reach = max(i + nums[i], max_reach);
+
+		return max_reach;
 	}
 public:
 	int jump(const vector<int>& nums) {
-
 		int n = nums.size();
-		vector<int> dp(n);
+		if(n == 1) return 0;
 
-		dp[n - 1] = 0;
-		for(int i = n - 2; i >= 0; --i) {
-			dp[i] = 1 + getMinimoRaggiungibile(nums, dp, i);
+		int curr_reach = nums[0];
+		int prev_reach = 0;
+		int jumps = 1;
+
+		while(curr_reach < n - 1) {
+			int new_reach = getMaxReach(nums, prev_reach, curr_reach);
+			prev_reach = curr_reach;
+			curr_reach = new_reach;
+
+			jumps++;
 		}
-
-		return dp[0];
+		return jumps;
 	}
 };
 // @lc code=end
 
+int main(int argc, char const* argv[])
+{
+	vector<int> nums = { 2, 3, 1, 1, 4 };
+	cout << Solution().jump(nums);
+	return 0;
+}
