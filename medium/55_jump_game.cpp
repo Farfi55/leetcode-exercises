6e11eb532710cd1ce2773a70e2235a6bf901bfd3
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-04 16:53:39 +0100

    [55] Jump Game (v. 3)

--- medium/55_jump_game.cpp
+++ medium/55_jump_game.cpp
@@ -13,17 +13,13 @@ class Solution {
 public:
 	bool canJump(vector<int>& nums) {
 		const int n = nums.size();
-		vector<bool> dp(n, false);
 
-		dp[0] = true;
-		for(int i = 0; i < n; i++) {
-			if(!dp[i]) return false;
-
-			for(int j = 1; j <= nums[i] && j + i < n; j++) {
-				dp[i + j] = true;
-			}
+		int jumps = nums[0];
+		int i;
+		for(i = 0; jumps && i < n; i++) {
+			jumps = max(nums[i], jumps - 1);
 		}
-		return dp[n - 1];
+		return (i == n) || (n == 1);
 	}
 };
 // @lc code=end
