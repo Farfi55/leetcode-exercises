c47c5800c5a9b9361c0cb48dbacb2607258620b3
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-05 14:29:23 +0100

    [53] Maximum Subarray (v. 3)

--- easy/53_maximum_subarray.cpp
+++ easy/53_maximum_subarray.cpp
@@ -19,7 +19,7 @@ public:
 
 		for(int i = 0; i < n; i++) {
 			curr_sum += nums[i];
-			if(curr_sum > max_sum) max_sum = curr_sum;
+			max_sum = max(max_sum, curr_sum);
 			if(curr_sum < 0) curr_sum = 0;
 		}
 		return max_sum;
