bc3bae4435d44e11bce8c4b71e7245f81b530a84
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-06 16:49:48 +0100

    [3] Longest Substring Without Repeating Characters (v. 1)

--- medium/3_longest_substring_without_repeating_characters.cpp
+++ medium/3_longest_substring_without_repeating_characters.cpp
@@ -6,31 +6,31 @@
 
 // @lc code=start
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        // last index char i appared in the string, 0 = never, 1 = 1° char and so on
-        int character_at[128] = { 0 };
-        int max_lenght = 0;
-        int current_lenght = 0;
-        for(int i = 0; i < s.size(); i++) {
-            const unsigned char c = s[i];
-            if(character_at[c] > i - current_lenght) {
-                current_lenght = i - (character_at[c] - 1);
-            }
-            else current_lenght++;
+	int lengthOfLongestSubstring(string s) {
+		// last index char i appared in the string
+		// -1 implies never (for now)
+		vector<int> character_at(128, -1);
+		int max_lenght = 0;
+		int current_lenght = 0;
 
-            character_at[c] = i + 1;
+		for(int i = 0; i < s.size(); i++) {
+			const unsigned char c = s[i];
 
-            if(current_lenght > max_lenght)
-                max_lenght = current_lenght;
-        }
+			current_lenght = min(current_lenght + 1, i - character_at[c]);
 
-        return max_lenght;
+			character_at[c] = i;
 
-    }
+			max_lenght = max(current_lenght, max_lenght);
+		}
+
+		return max_lenght;
+
+	}
 };
 // @lc code=end
 
