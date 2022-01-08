2dc111b63ff9a939b48a1ef4da76c0bcf41c3dd6
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-08 17:17:17 +0100

    [122] Best Time to Buy and Sell Stock II (v. 2)

--- medium/122_best_time_to_buy_and_sell_stock_ii.cpp
+++ medium/122_best_time_to_buy_and_sell_stock_ii.cpp
@@ -17,16 +17,14 @@ class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
 		int n = prices.size();
-
 		int profit = 0;
 
-		int i = 0;
-		while(i < n) {
+		for(int i = 0; i < n; i++) {
 			while(i + 1 < n && prices[i + 1] <= prices[i])i++;
 			int buy_price = prices[i];
 
 			while(i + 1 < n && prices[i + 1] >= prices[i])i++;
-			int sell_price = prices[i++];
+			int sell_price = prices[i];
 
 			profit += sell_price - buy_price;
 		}
