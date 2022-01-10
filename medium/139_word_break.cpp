88e3c1239eef06c5c6987ad2134fbbdda14842a0
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-11 00:43:15 +0100

    [139] Word Break v01 too slow

---
+++ medium/139_word_break.cpp
@@ -0,0 +1,54 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+// #include "commoncppproblem139.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=139 lang=cpp
+ *
+ * [139] Word Break
+ */
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+	unordered_set<string> dict;
+
+
+	bool rec(string s, int start) {
+		if(start == s.length()) return true;
+
+		for(int len = s.length() - start; len > 0 ; --len) {
+			if(dict.find(s.substr(start, len)) != dict.end()) // &&
+				if(rec(s, start + len) == true)
+					return true;
+		}
+		return false;
+	}
+
+
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		for(string& word : wordDict)
+			dict.insert(word);
+
+		return rec(s, 0);
+	}
+};
+// @lc code=end
+
+// int main(int argc, char const* argv[])
+// {
+// 	vector<string> a{ "leet", "code" };
+// 	cout << Solution().wordBreak("leetcode", a);
+// 	return 0;
+// }
