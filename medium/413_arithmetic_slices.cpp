5e25fd7191facd8e86f555cdccb16d9a11db55d4
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-11 00:01:07 +0100

    [413] Arithmetic Slices v02

--- medium/413_arithmetic_slices.cpp
+++ medium/413_arithmetic_slices.cpp
@@ -7,21 +7,14 @@
 // @lc code=start
 class Solution {
 public:
-	int numberOfArithmeticSlices(vector<int>& nums) {
-		const int n = nums.size();
-		int total = 0;
-		int count = 0; // 1 because we ignore the first element 
-		int curr_jump = INT_MIN;
+	int numberOfArithmeticSlices(vector<int>& a) {
+		const int n = a.size();
+		int total = 0, count = 0;
 
-		for(int i = 1; i < n; i++) {
-			if(nums[i] - nums[i - 1] == curr_jump) {
-				if(++count > 2)
-					total += (count - 2);
-			}
-			else {
-				count = 2;
-				curr_jump = nums[i] - nums[i - 1];
-			}
+		for(int i = 2; i < n; ++i) {
+			if(a[i] - a[i - 1] == a[i - 1] - a[i - 2])
+				total += ++count;
+			else count = 0;
 		}
 
 		return total;
