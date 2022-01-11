489497e1a996c520aa0e2a68b2440f32c6cc9db7
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-11 19:00:10 +0100

    [102] Binary Tree Level Order Traversal v01

---
+++ medium/102_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,45 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+// #include "commoncppproblem102.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=102 lang=cpp
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+	void dfs(TreeNode* node, int depth, vector<vector<int>>& out) {
+		if(!node) return;
+
+		if(depth >= out.size()) out.push_back(vector<int>{node->val});
+		else out[depth].push_back(node->val);
+
+		dfs(node->left, depth + 1, out);
+		dfs(node->right, depth + 1, out);
+	}
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> out;
+		dfs(root, 0, out);
+		return out;
+	}
+};
+// @lc code=end
+
