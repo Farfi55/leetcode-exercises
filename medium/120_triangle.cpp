51d3ab9f25637e62957d3742206ac632f3aefc75
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-12 15:59:07 +0100

    [120] Triangle v01

---
+++ medium/120_triangle.cpp
@@ -0,0 +1,29 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+// #include "commoncppproblem120.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=120 lang=cpp
+ *
+ * [120] Triangle
+ */
+
+// @lc code=start
+class Solution {
+public:
+	int minimumTotal(vector<vector<int>>& triangle) {
+		int n = triangle.size();
+
+		for(int i = n - 2; i >= 0; i--)
+			for(int j = 0; j <= i; j++)
+				triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+
+		return triangle[0][0];
+	}
+};
+// @lc code=end
+
