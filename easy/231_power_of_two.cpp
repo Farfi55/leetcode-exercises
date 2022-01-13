3d36027732fb858f83ec7346a55c211cf615b800
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-13 14:33:09 +0100

    [231] Power of Two v02 using bit operations

--- easy/231_power_of_two.cpp
+++ easy/231_power_of_two.cpp
@@ -4,18 +4,27 @@
  * [231] Power of Two
  */
 
+
+// 7: 0000 0111
+// 8: 0000 1000
+// 9: 0000 1001
+
+// 7: 		0000 0111
+// 8: 		0000 1000
+// 8 & 7: 	0000 0000
+
+// 9 & 8: 	0000 1000
+
+// when we AND the bits of a power of 2 
+// with itself -1 we get a 'all 0' bitstring
+// we can use this to check if a number is a power of 2
+
 // @lc code=start
 class Solution {
 public:
 	bool isPowerOfTwo(int n) {
 		if(n <= 0) return false;
-
-		int counter = 0;
-		for(int i = 0; i < 32; i++) {
-			counter += (n & 1);
-			n >>= 1;
-		}
-		return counter == 1;
+		return (n & (n - 1) == 0);
 	}
 };
 // @lc code=end
