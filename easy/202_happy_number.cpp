32dad311c26d30d5057c36a52c6bf8267836b2b7
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-13 20:27:10 +0100

    [202] Happy Number v02

--- easy/202_happy_number.cpp
+++ easy/202_happy_number.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+// #include "commoncppproblem202.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=202 lang=cpp
  *
@@ -6,19 +14,26 @@
 
 // @lc code=start
 class Solution {
-public:
-	int max_depth = 32;
-	bool isHappy(int n) {
-		int sum = 0;
-
+	int to_happy(int n) {
+		int ans = 0;
 		while(n) {
-			sum += (n % 10) * (n % 10);
+			ans += (n % 10) * (n % 10);
 			n /= 10;
 		}
-
-		if(sum == 1) return true;
-		else if(!max_depth--)return false;
-		return isHappy(sum);
+		return ans;
+	}
+public:
+	int max_depth = 32;
+	bool isHappy(int x) {
+		bool arr[1000] = { false };
+		x = to_happy(x);
+		while(!arr[x]) {
+			arr[x] = true;
+			if(x == 1)
+				return true;
+			x = to_happy(x);
+		}
+		return false;
 	}
 };
 // @lc code=end
