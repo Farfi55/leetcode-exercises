00fba09c9be3a8a99ab9604a1e3ead0494ca82c2
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-14 22:54:42 +0100

    [1314] Matrix Block Sum v02

--- medium/1314_matrix_block_sum.cpp
+++ medium/1314_matrix_block_sum.cpp
@@ -23,38 +23,67 @@ con k = 2
 	[45, 45, 45]
 	[45, 45, 45]
 	[45, 45, 45]
+
+	[ 1,  3,  6]
+	[ 5, 12, 21]
+	[12, 27, 45]
+
+
+
+
+[1, 2, 3, 3]
+[7, 1, 2, 5]
+[4, 5, 6, 8]
+[7, 8, 9, 2]
+
+
+
+
+
+
 */
+
+
+
 // @lc code=start
 
 class Solution {
+	int get(vector<vector<int>>& sums, int i, int j) {
+		if(i < 0 || j < 0) return 0;
+
+		i = min(i, int(sums.size()) - 1);
+		j = min(j, int(sums[0].size()) - 1);
+
+		return sums[i][j];
+	}
+
 public:
 	vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
 		int m = mat.size();
 		int n = mat[0].size();
 
-		vector<int> row_sum(m, 0);
-		vector<int> col_sum(n, 0);
-		int tot_sum = 0;
+		vector<vector<int>> sums(m, vector<int>(n, 0));
 
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				row_sum[i] += mat[i][j];
-				col_sum[j] += mat[i][j];
-				tot_sum += mat[i][j];
+		sums[0][0] = mat[0][0];
+		for(int j = 1; j < n; j++)
+			sums[0][j] = sums[0][j - 1] + mat[0][j];
+
+		for(int i = 1; i < m; i++) {
+			sums[i][0] = sums[i - 1][0] + mat[i][0];
+			for(int j = 1; j < n; j++) {
+				sums[i][j] = mat[i][j] +
+					sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1];
 			}
 		}
 
 		for(int i = 0; i < m; i++) {
 			for(int j = 0; j < n; j++) {
-				mat[i][j] = 0;
-				for(int l = -k; l <= k; l++) {
-					if(0 <= i + l && i + l < m)
-						mat[i][j] += row_sum[i + l];
-					if(0 <= j + l && j + l < n)
-						mat[i][j] += col_sum[j + l];
-				}
+				mat[i][j] = get(sums, i + k, j + k) // -
+					- get(sums, i - k - 1, j + k) - get(sums, i + k, j - k - 1) // +
+					+ get(sums, i - k - 1, j - k - 1);
 			}
 		}
+
 		return mat;
 	}
 };
