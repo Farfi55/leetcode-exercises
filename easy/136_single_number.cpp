97363fc7c97399b1aea46111dd13e3e9daf7f242
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-14 18:54:15 +0100

    [136] Single Number v01 O(n) using xor

--- easy/136_single_number.cpp
+++ easy/136_single_number.cpp
@@ -14,13 +14,12 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-	int singleNumber(vector<int>& nums) {
-		sort(nums.begin(), nums.end());
-		for(int i = 0; i < nums.size() - 1; i += 2) {
-			if(nums[i] != nums[i + 1])
-				return nums[i];
-		}
-		return nums[nums.size() - 1];
+	int singleNumber(const vector<int>& nums) {
+		int ret = 0;
+		for(const int num : nums)
+			ret ^= num;
+
+		return ret;
 	}
 };
 // @lc code=end
