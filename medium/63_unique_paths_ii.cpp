4e1a46d84de765c8362a9afbd92ccf5fbe6fbd8f
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-15 15:45:06 +0100

    [63] Unique Paths II v03 O(n*m) time O(n) extra space

--- medium/63_unique_paths_ii.cpp
+++ medium/63_unique_paths_ii.cpp
@@ -12,56 +12,6 @@ using namespace std;
  * [63] Unique Paths II
  */
 
-/*
-
-	. . . . .
-	. . . . .
-	. * . . .
-	. . . . .
-
-	+-+ . . .
-	| | . . .
-	+-+-----+
-	. +-----+
-
-	[0,0,0,0],
-	[0,1,0,0],
-	[0,0,0,0],
-	[0,0,1,0],
-	[0,0,0,0]
-
-	. . . .
-	. * . .
-	. . . .
-	. . * .
-	. . . .
-
-	+-+ . .
-	+-+---+
-	. | . |
-	. | * |
-	. +---+
-
-	+---+ .
-	| * | .
-	| . | .
-	+---+-+
-	. . +-+
-
-	+-+ . .
-	+-+-+ .
-	. | | .
-	. +-+-+
-	. . +-+
-
-
-*/
-
-#include <cmath>
-#include <vector>
-
-using namespace std;
-
 // @lc code=start
 class Solution {
 public:
@@ -69,26 +19,19 @@ public:
 		const size_t m = obstacleGrid.size();
 		const size_t n = obstacleGrid[0].size();
 
-		vector<vector<int>> dp(m, vector<int>(n, 1));
+		vector<int> dp(n, 0);
+		dp[0] = 1;
 
-
-		dp[0][0] = 1 - obstacleGrid[0][0];
-		for(int j = 1; j < n; j++)
-			dp[0][j] = min(1 - obstacleGrid[0][j], dp[0][j - 1]);
-
-		for(int i = 1; i < m; i++) {
-			dp[i][0] = min(1 - obstacleGrid[i][0], dp[i - 1][0]);
-			for(int j = 1; j < n; j++) {
+		for(size_t i = 0; i < m; i++) {
+			dp[0] = min(dp[0], 1 - obstacleGrid[i][0]);
+			for(size_t j = 1; j < n; j++) {
 				if(!obstacleGrid[i][j])
-					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-				else dp[i][j] = 0;
+					dp[j] += dp[j - 1];
+				else dp[j] = 0;
 			}
 		}
 
-		return dp[m - 1][n - 1];
+		return dp[n - 1];
 	}
-
-
 };
 // @lc code=end
-
