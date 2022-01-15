0bfdb108c1af6150aa0ab002d639815ee4a21d45
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-15 15:26:16 +0100

    [63] Unique Paths II v02 dynamic programming

--- medium/63_unique_paths_ii.cpp
+++ medium/63_unique_paths_ii.cpp
@@ -12,30 +12,6 @@ using namespace std;
  * [63] Unique Paths II
  */
 
-#include <cmath>
-#include <vector>
-
-using namespace std;
-
-// @lc code=start
-class Solution {
-
-	uint64_t uniquePaths(int m, int n) {
-
-		// make sure that m >= n
-		if(m < n) return uniquePaths(n, m);
-
-
-		uint64_t res = 1;
-
-		for(int i = m + 1, j = 1; j <= n; i++, j++) {
-			res *= i;
-			res /= j;
-		}
-		return res;
-	}
-
-
 /*
 
 	. . . . .
@@ -80,33 +56,39 @@ class Solution {
 
 
 */
+
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
 public:
 	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 		const size_t m = obstacleGrid.size();
 		const size_t n = obstacleGrid[0].size();
 
-		vector<pair<int, int>> obstacles;
-
-		for(int i = 0; i < m; i++)
-			for(int j = 0; j < n; j++)
-				if(obstacleGrid[i][j]) {
-					if((i == 0 && j == 0) ||
-						(i == m - 1 && j == n - 1))
-						return 0;
-					obstacles.push_back(make_pair(i, j));
-				}
+		vector<vector<int>> dp(m, vector<int>(n, 1));
 
 
-		uint64_t total = uniquePaths(m - 1, n - 1);
+		dp[0][0] = 1 - obstacleGrid[0][0];
+		for(int j = 1; j < n; j++)
+			dp[0][j] = min(1 - obstacleGrid[0][j], dp[0][j - 1]);
 
-		for(int k = 0; k < obstacles.size();k++) {
-			int i = obstacles[k].first;
-			int j = obstacles[k].second;
-			total -= uniquePaths(i, j) * uniquePaths(m - i - 1, n - j - 1);
+		for(int i = 1; i < m; i++) {
+			dp[i][0] = min(1 - obstacleGrid[i][0], dp[i - 1][0]);
+			for(int j = 1; j < n; j++) {
+				if(!obstacleGrid[i][j])
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				else dp[i][j] = 0;
+			}
 		}
 
-		return max((int)total, 0);
+		return dp[m - 1][n - 1];
 	}
+
+
 };
 // @lc code=end
 
