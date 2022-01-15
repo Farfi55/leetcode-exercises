4d910c0434ad193f939b013384385105ee15cc1f
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-15 14:16:05 +0100

    [62] Unique Paths v03 using math, but factorial grows too fast

--- medium/62_unique_paths.cpp
+++ medium/62_unique_paths.cpp
@@ -10,26 +10,24 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-	// vector<int> fact_dp{ 1, 1 };
-	// int fact(int n) {
-	// 	if(n >= fact_dp.size())
-	// 		fact_dp.push_back(n * fact(n - 1));
+	vector<uint64_t> fact_dp{ 1, 1 };
+	uint64_t fact(uint64_t n) {
+		if(n >= fact_dp.size())
+			fact_dp.push_back(n * fact(n - 1));
 
-	// 	return fact_dp[n];
-	// }
-	// int comb(int n, int k) {
-	// 	return fact(n) / (fact(n - k) * fact(k));
-	// }
+		return fact_dp[n];
+	}
+	uint64_t comb(uint64_t n, uint64_t k) {
+		return fact(n) / (fact(n - k) * fact(k));
+	}
+
+	uint64_t perm(uint64_t n, uint64_t k) {
+		return (fact(n + k)) / (fact(k) * fact(n));
+	}
 
 public:
 	int uniquePaths(int m, int n) {
-		vector<int> dp(n, 1);
-
-		for(int i = 1; i < m; i++)
-			for(int j = 1; j < n; j++)
-				dp[j] += dp[j - 1];
-
-		return dp[n - 1];
+		return perm(n - 1, m - 1);
 	}
 };
 // @lc code=end
