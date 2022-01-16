fc4aa7d4d6dde04cac42a378f6a059896120d104
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-16 15:07:48 +0100

    [221] Maximal Square v03 O(n*m) time O(n) space using O(1) space in c++ is impossible because the answer for some cases exceeds char's value range

--- medium/221_maximal_square.cpp
+++ medium/221_maximal_square.cpp
@@ -21,18 +21,18 @@ public:
 
 		int max_sqr = 0;
 
-		vector<vector<int>> dp(m, vector<int>(n, 0));
-		for(int i = 0; i < m; i++)
-			for(int j = 0; j < n; j++) {
-				dp[i][j] = M[i][j] - '0';
-				max_sqr = max(max_sqr, dp[i][j]);
-			}
+		vector<vector<int>> dp(2, vector<int>(n + 1, 0));
 
-		for(int i = m - 2; i >= 0; i--) {
-			for(int j = n - 2; j >= 0; j--) {
-				if(dp[i][j])
-					dp[i][j] = 1 + min(dp[i + 1][j + 1], min(dp[i][j + 1], dp[i + 1][j]));
-				max_sqr = max(max_sqr, dp[i][j]);
+		for(int i = m - 1; i >= 0; i--) {
+			for(int j = n - 1; j >= 0; j--) {
+				// i & 1 or (i+1)&1 basically alternates between 2 dp
+				// arrays for every i, so we don't need to store m of them but just 2
+				// (i & 1) its the same as writing (i % 2) for integers
+				if(M[i][j] == '0')
+					dp[i & 1][j] = 0;
+				else // dp(i,j) = 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
+					dp[i & 1][j] = 1 + min(dp[(i + 1) & 1][j + 1], min(dp[i & 1][j + 1], dp[(i + 1) & 1][j]));
+				max_sqr = max(max_sqr, dp[i & 1][j]);
 			}
 		}
 		return max_sqr * max_sqr;
