7c6aa476bf537824631936f0e1db9a67033b02e2
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-18 14:27:06 +0100

    [240] Search a 2D Matrix II v04 O(n+m) time

--- medium/240_search_a_2_d_matrix_ii.cpp
+++ medium/240_search_a_2_d_matrix_ii.cpp
@@ -14,35 +14,33 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-	bool bin_search(vector<int>& arr, int target, int i, int j) {
-		while(i <= j) {
-			int mid = (i + j) / 2;
+	// bool bin_search(vector<int>& arr, int target, int i, int j) {
+	// 	while(i <= j) {
+	// 		int mid = (i + j) / 2;
 
-			if(target > arr[mid]) i = mid + 1;
-			else if(target < arr[mid]) j = mid - 1;
+	// 		if(target > arr[mid]) i = mid + 1;
+	// 		else if(target < arr[mid]) j = mid - 1;
+	// 		else return true;
+	// 	}
+	// 	return false;
+	// }
+public:
+	bool searchMatrix(vector<vector<int>>& M, int target) {
+		const auto m = M.size();
+
+		int i = 0;
+		int j = M[0].size() - 1;
+
+		while(i < m && j >= 0) {
+			// all elements in this colum are greater, skip column
+			if(M[i][j] > target) j--;
+			// remaining elements in this row are all less than target, skip row
+			else if(M[i][j] < target) i++;
+			// we found the target
 			else return true;
 		}
 		return false;
-	}
-public:
-	bool searchMatrix(vector<vector<int>>& M, int target) {
-		int m = M.size();
-		int n = M[0].size();
 
-
-		int left = n - 1;
-
-		while(left >= 0 && M[0][left] > target)
-			--left;
-
-		for(int i = 0; i < m; i++) {
-			if(M[i][0] > target)
-				return false;
-
-			if(bin_search(M[i], target, 0, left))
-				return true;
-		}
-		return false;
 	}
 };
 // @lc code=end
