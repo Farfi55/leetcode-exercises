8d7cbbab83724bfc29cb4f9a032468cedec05d30
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-22 11:51:54 +0100

    [49] Group Anagrams v02 O(n) time O(n) space

--- medium/49_group_anagrams.cpp
+++ medium/49_group_anagrams.cpp
@@ -5,66 +5,46 @@
  */
 
 #include <vector>
+#include <array>
 #include <string>
 #include <unordered_map>
 
 using namespace std;
 
 // @lc code=start
+struct ArrayHasher {
+	// from https://stackoverflow.com/a/42701911/6075271
+
+	std::size_t operator()(const std::array<int, 26>& a) const {
+		std::size_t h = 0;
+
+		for(auto e : a) {
+			h ^= std::hash<int>{}(e)+0x9e3779b9 + (h << 6) + (h >> 2);
+		}
+		return h;
+	}
+};
+
 class Solution {
 public:
 	vector<vector<string>> groupAnagrams(vector<string>& strs) {
-		int letters[26] = { 0 };
-		vector<bool> used(strs.size(), false);
-
+		unordered_map<array<int, 26>, int, ArrayHasher> map;
 		vector<vector<string>> out;
 
 		for(int i = 0; i < strs.size(); i++) {
-			if(used[i])
-				continue;
-
-			string& str_base = strs[i];
-			out.push_back({ str_base });
-			used[i] = true;
-
-			for(int j = 0; j < str_base.length(); j++) {
-				letters[str_base[j] - 'a']++;
+			array<int, 26> letters;
+			letters.fill(0);
+			for(char& c : strs[i]) {
+				letters[c - 'a']++;
 			}
 
-
-			for(int k = 0; k < strs.size(); k++) {
-				if(used[k])
-					continue;
-				string& str_current = strs[k];
-				for(int l = 0; l < str_current.length(); l++)
-					letters[str_current[l] - 'a']--;
-
-
-				bool all_zero = true;
-
-				for(int m = 0; m < 26 && all_zero; m++)
-					if(letters[m] != 0)
-						all_zero = false;
-
-
-				if(all_zero) {
-					out.back().push_back(str_current);
-					used[k] = true;
-				}
-
-
-				for(int l = 0; l < str_current.length(); l++)
-					letters[str_current[l] - 'a']++;
-
+			if(map.find(letters) == map.end()) {
+				map[letters] = out.size();
+				out.push_back({ strs[i] });
 			}
-
-
-
-
-			for(int j = 0; j < str_base.length(); j++) {
-				letters[str_base[j] - 'a']--;
+			else {
+				out[map[letters]].push_back(strs[i]);
 			}
-
 		}
 
 		return out;
