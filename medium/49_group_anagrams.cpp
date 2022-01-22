1ad00b3fb443138f477a8b30cc52fb1d3c6018fd
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-22 12:12:00 +0100

    [49] Group Anagrams v03 O(n) time O(n) space using strings as key

--- medium/49_group_anagrams.cpp
+++ medium/49_group_anagrams.cpp
@@ -11,39 +11,45 @@
 
 using namespace std;
 
+// struct ArrayHasher {
+// 	// from https://stackoverflow.com/a/42701911/6075271
+
+// 	std::size_t operator()(const std::array<int, 26>& a) const {
+// 		std::size_t h = 0;
+
+// 		for(auto e : a) {
+// 			h ^= std::hash<int>{}(e)+0x9e3779b9 + (h << 6) + (h >> 2);
+// 		}
+// 		return h;
+// 	}
+// };
+
 // @lc code=start
-struct ArrayHasher {
-	// from https://stackoverflow.com/a/42701911/6075271
-
-	std::size_t operator()(const std::array<int, 26>& a) const {
-		std::size_t h = 0;
-
-		for(auto e : a) {
-			h ^= std::hash<int>{}(e)+0x9e3779b9 + (h << 6) + (h >> 2);
-		}
-		return h;
-	}
-};
 
 class Solution {
 public:
 	vector<vector<string>> groupAnagrams(vector<string>& strs) {
-		unordered_map<array<int, 26>, int, ArrayHasher> map;
+		unordered_map<string, int> map;
 		vector<vector<string>> out;
 
 		for(int i = 0; i < strs.size(); i++) {
-			array<int, 26> letters;
-			letters.fill(0);
+			int letters[26] = { 0 };
 			for(char& c : strs[i]) {
 				letters[c - 'a']++;
 			}
 
-			if(map.find(letters) == map.end()) {
-				map[letters] = out.size();
+			string key;
+			key.reserve(strs[i].length());
+			for(int j = 0; j < 26; j++)
+				key += string(letters[j], j + 'a');
+
+
+			if(map.find(key) == map.end()) {
+				map[key] = out.size();
 				out.push_back({ strs[i] });
 			}
 			else {
-				out[map[letters]].push_back(strs[i]);
+				out[map[key]].push_back(strs[i]);
 			}
 		}
 
