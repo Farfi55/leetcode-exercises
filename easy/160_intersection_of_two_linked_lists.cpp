1b7fdaa56d88582d2f57c94360097d9b9f919e90
Alessio Farfaglia <djalexy555@gmail.com>
2022-01-25 10:44:58 +0100

    [160] Intersection of Two Linked Lists v02

--- easy/160_intersection_of_two_linked_lists.cpp
+++ easy/160_intersection_of_two_linked_lists.cpp
@@ -21,21 +21,36 @@ using namespace std;
  */
 class Solution {
 public:
-	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-		unordered_set<ListNode*> nodes;
+	// from https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/49789/My-accepted-simple-and-shortest-C++-code-with-comments-explaining-the-algorithm.-Any-comments-or-improvements
 
-		while(headA) {
-			nodes.insert(headA);
-			headA = headA->next;
+	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+	{
+		ListNode* p1 = headA;
+		ListNode* p2 = headB;
+
+		if(p1 == NULL || p2 == NULL) return NULL;
+
+		while(p1 != NULL && p2 != NULL && p1 != p2) {
+			p1 = p1->next;
+			p2 = p2->next;
+
+			//
+			// Any time they collide or reach end together without colliding 
+			// then return any one of the pointers.
+			//
+			if(p1 == p2) return p1;
+
+			//
+			// If one of them reaches the end earlier then reuse it 
+			// by moving it to the beginning of other list.
+			// Once both of them go through reassigning, 
+			// they will be equidistant from the collision point.
+			//
+			if(p1 == NULL) p1 = headB;
+			if(p2 == NULL) p2 = headA;
 		}
 
-		while(headB) {
-			if(nodes.count(headB))
-				return headB;
-			headB = headB->next;
-		}
-
-		return nullptr;
+		return p1;
 	}
 };
 // @lc code=end
