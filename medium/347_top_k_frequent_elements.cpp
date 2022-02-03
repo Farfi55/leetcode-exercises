4c6d2163f07761dc024405007cded84cb703d3f8
Alessio Farfaglia <djalexy555@gmail.com>
2022-02-03 13:21:55 +0100

    [347] Top K Frequent Elements v02 using Heap

--- medium/347_top_k_frequent_elements.cpp
+++ medium/347_top_k_frequent_elements.cpp
@@ -12,28 +12,70 @@ using namespace std;
 
 // @lc code=start
 class Solution {
+	void heapify(vector<pair<int, int>>& heap, int i) {
+		int largest = i;
+		int l = 2 * i; // left = 2*i
+		int r = 2 * i + 1; // right = 2*i + 1
+
+		// If left child is larger than root
+		if(l < heap.size() && heap[l].second > heap[largest].second)
+			largest = l;
+
+		 // If right child is larger than largest so far
+		if(r < heap.size() && heap[r].second > heap[largest].second)
+			largest = r;
+
+		// If largest is not root
+		if(largest != i) {
+			swap(heap[i], heap[largest]);
+
+			// Recursively heapify the affected sub-tree
+			heapify(heap, largest);
+		}
+
+	}
+
+
 public:
 	vector<int> topKFrequent(vector<int>& nums, int k) {
 		unordered_map<int, int> frequencies;
 
+		int uniques = 0;
 		for(int x : nums) {
-			frequencies[x]++;
+			if(frequencies[x]++)
+				uniques++;
 		}
 
-		vector<pair<int, int>> elems;
+		// we start at index 1 just to make index operation simpler
+		vector<pair<int, int>> heap(1);
+		heap.reserve(uniques + 1);
 
-		for(auto pair_ : frequencies) {
-			elems.emplace_back(pair_.second, pair_.first);
+		for(auto& frequence : frequencies) {
+			int i = heap.size();
+			heap.emplace_back(frequence);
+
+			// while this new frequence is higher than its parent
+			while(i > 1 && heap[i].second > heap[i / 2].second) {
+				swap(heap[i], heap[i / 2]);
+				i /= 2;
+			}
 		}
 
-		sort(elems.begin(), elems.end());
 
-		vector<int> result;
+		vector<int> out; out.reserve(k);
 
-		for(int i = 1; i <= k; i++)
-			result.push_back(elems[elems.size() - i].second);
+		for(int i = 0; i < k; i++) {
+			out.push_back(heap[1].first);
+			heap[1] = heap.back();
+			heap.pop_back();
 
-		return result;
+			heapify(heap, 1);
+
+		}
+
+		return out;
+
+
 
 	}
 };
