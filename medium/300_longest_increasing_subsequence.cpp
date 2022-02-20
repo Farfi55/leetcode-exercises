f67e5393b93b50a0cf720b8bd145b407d89c16bd
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-02-20 14:05:39 +0100

    [300] Longest Increasing Subsequence v02

--- medium/300_longest_increasing_subsequence.cpp
+++ medium/300_longest_increasing_subsequence.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem300.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=300 lang=cpp
  *
@@ -5,29 +13,25 @@
  */
 
 #include <vector>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
 
 // @lc code=start
 class Solution {
-
-	int lengthOfLIS(vector<int>& nums, int i, int prev) {
-		if(i >= nums.size())
-			return 0;
-		else if(nums[i] > prev) {
-			return max(1 + lengthOfLIS(nums, i + 1, nums[i]), lengthOfLIS(nums, i + 1, prev));
-		}
-		else
-			return lengthOfLIS(nums, i + 1, prev);
-
-		// if we dont pick the i-th num
-
-	}
-
 public:
 	int lengthOfLIS(vector<int>& nums) {
-		return lengthOfLIS(nums, 0, INT_MIN);
+		int n = nums.size();
+
+		vector<int> dp(n, 1);
+		for(int i = 0; i < n; ++i)
+			for(int j = 0; j < i; ++j)
+				if(nums[i] > nums[j] && dp[i] < dp[j] + 1)
+					dp[i] = dp[j] + 1;
+
+		return *max_element(dp.begin(), dp.end());
 	}
 };
 // @lc code=end
