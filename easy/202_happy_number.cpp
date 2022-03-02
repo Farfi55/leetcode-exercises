116483161b722fbb19ce46cacd70f9e57ba21cd4
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-03-02 16:38:39 +0100

    added some comments to explain what's happening

--- easy/202_happy_number.cpp
+++ easy/202_happy_number.cpp
@@ -17,21 +17,29 @@ class Solution {
 	int to_happy(int n) {
 		int ans = 0;
 		while(n) {
+			// sum the square of the last digit 
 			ans += (n % 10) * (n % 10);
 			n /= 10;
 		}
 		return ans;
 	}
 public:
-	int max_depth = 32;
 	bool isHappy(int x) {
+		// since n is at most 2^31 = 2.147.483.648 
+		// to_happy can at most be to_happy(1.999.999.999)
+		// wich is 1^2 + 9*(9^2) = 730
+		// so we can keep track of all 
 		bool arr[1000] = { false };
+
 		x = to_happy(x);
-		while(!arr[x]) {
-			arr[x] = true;
+
+		while(!arr[x]) { // while there is no cycles
+			arr[x] = true; // store the new occurrance of x
+
 			if(x == 1)
 				return true;
-			x = to_happy(x);
+
+			x = to_happy(x); // get next happy number
 		}
 		return false;
 	}
