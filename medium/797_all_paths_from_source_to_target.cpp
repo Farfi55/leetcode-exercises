393dddbbf3adb34ff035321490cda5fc4e774e74
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-03-09 00:32:05 +0100

    [797] All Paths From Source to Target v02 same stuff but less argument passing

--- medium/797_all_paths_from_source_to_target.cpp
+++ medium/797_all_paths_from_source_to_target.cpp
@@ -9,24 +9,27 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-    void solve(int node, vector<int>& currPath, vector<vector<int>>& graph, vector<vector<int>>& paths) {
+
+    vector<int> currPath;
+    vector<vector<int>> paths;
+
+
+    void solve(int node, vector<vector<int>>& graph) {
         currPath.push_back(node);
-        if(node == graph.size() - 1) {
+
+        if(node == graph.size() - 1)
             paths.push_back(currPath);
-        }
-        else for(int i = 0; i < graph[node].size(); i++) {
-            solve(graph[node][i], currPath, graph, paths);
-        }
+        else for(auto next_node : graph[node])
+            solve(next_node, graph);
 
         currPath.pop_back();
     }
 
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> paths;
-        vector<int> currPath; currPath.reserve(graph.size());
+        currPath.reserve(graph.size());
 
-        solve(0, currPath, graph, paths);
+        solve(0, graph);
         return paths;
     }
 };
