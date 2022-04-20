3c5f46528ec5c459bc742f61d1eda83d22ec2ba4
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-04-20 10:17:14 +0200

    [608] Tree Node v02

--- medium/608_tree_node.sql
+++ medium/608_tree_node.sql
@@ -9,11 +9,11 @@
 -- Write your MySQL query statement below
 
 SELECT id,
-IF (p_id IS NULL, "Root", 
-    IF(id IN (
-        SELECT p_id
-        FROM Tree 
-    ), "Inner", "Leaf")) AS type
+CASE
+    WHEN p_id IS NULL THEN "Root"
+    WHEN id IN ( SELECT p_id FROM Tree) THEN "Inner"
+    ELSE "Leaf"
+END AS type
 FROM Tree
 ORDER BY id
 -- @lc code=end
