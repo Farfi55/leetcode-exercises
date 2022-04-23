cda195ad7af8f0a277dbd70016920e1ae8b358fe
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-04-23 13:00:27 +0200

    [1141] User Activity for the Past 30 Days I v01

---
+++ easy/1141_user_activity_for_the_past_30_days_i.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=1141 lang=mysql
+--
+-- [1141] User Activity for the Past 30 Days I
+--
+
+
+
+-- @lc code=start
+-- Write your MySQL query statement below
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30
+GROUP BY activity_date
+
+
+-- @lc code=end
+
